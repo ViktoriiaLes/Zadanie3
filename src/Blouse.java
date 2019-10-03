83941d6352f80d66380e956c5b6f09244fa10b77
ViktoriiaLes <gutaatug@gmail.com>
2019-10-03 21:07:46 +0200

    First commit task 3 (exercise 1.1)

---
+++ src/Blouse.java
@@ -0,0 +1,12 @@
+public class Blouse {
+    String color;
+    String madeOf;
+    int size;
+
+    public Blouse() {
+        System.out.println("Call of default constructor Blouse");
+        color = "white";
+        madeOf = "cotton";
+        size = 40;
+    }
+}
